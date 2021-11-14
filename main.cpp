03155f1ba8ddf61510b5ac6b07a77606bd7c07ba
Cristiano4702 <91879010+Cristiano4702@users.noreply.github.com>
2021-11-14 17:24:12 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,24 @@
 using namespace std;
 
 int main() {
+    int n, a=1, b=1, c;
+    cin >> n;
+
+    if(n>=2)
+    {
+        cout << a << endl;
+        cout << b << endl;
+        for(int i=2; i<n; i++)
+        {
+            c = a+b;
+            cout << c << endl;
+            a=b;
+            b=c;
+        }
+    }
+    else
+    {
+        cout << "errore" << endl;
+    }
    return 0;
 }
